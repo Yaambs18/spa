00af31616c8026066d2ec6fce83a867ae56ca5d4
Yaambs18 <64511677+Yaambs18@users.noreply.github.com>
2021-08-31 08:05:12 +0530

    Update spa.js

--- spa.js
+++ spa.js
@@ -1,11 +1,7 @@
 const app = {
     pages: [],
-    show: new Event('show'),
     init: function(){
         app.pages = document.querySelectorAll('.page');
-        app.pages.forEach((pg)=>{
-            pg.addEventListener('show', app.pageShown);
-        })
         
         document.querySelectorAll('.nav-link').forEach((link)=>{
             link.addEventListener('click', app.nav);
@@ -20,23 +16,14 @@ const app = {
         document.getElementById(currentPage).classList.add('active');
         console.log(currentPage)
         history.pushState({}, currentPage, `#${currentPage}`);
-        document.getElementById(currentPage).dispatchEvent(app.show);
     },
-    pageShown: function(ev){
-        console.log('Page', ev.target.id, 'just shown');
-        let h1 = ev.target.querySelector('h1');
-        h1.classList.add('big')
-        setTimeout((h)=>{
-            h.classList.remove('big');
-        }, 1200, h1);
-    },
+    
     poppin: function(ev){
         console.log(location.hash, 'popstate event');
         let hash = location.hash.replace('#' ,'');
         document.querySelector('.active').classList.remove('active');
         document.getElementById(hash).classList.add('active');
         console.log(hash)
-        document.getElementById(hash).dispatchEvent(app.show);
     }
 }
 
